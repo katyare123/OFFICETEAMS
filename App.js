1f907313baee7c920ad9dfffd3c2d05cd22317e1
katyare123 <katyareparth1@gmail.com>
2025-02-18 13:28:29 +0530

    Update project files and configurations

--- App.js
+++ App.js
@@ -1,11 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import { WebView } from 'react-native-webview';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <WebView 
+        source={{ uri: 'https://parthoffice-teams.netlify.app/' }} 
+        style={styles.webview}
+        javaScriptEnabled={true}
+        domStorageEnabled={true}
+        setSupportMultipleWindows={false}
+        allowsFullscreenVideo={true}
+        allowsBackForwardNavigationGestures={true}
+        injectedJavaScript={`(function() {
+          if (document.documentElement.requestFullscreen) {
+            document.documentElement.requestFullscreen();
+          }
+          if (navigator.userAgent.match(/(iPhone|iPod|iPad|Android)/)) {
+            document.body.style.margin = 0;
+            document.documentElement.style.height = '100%';
+            document.body.style.height = '100%';
+            document.body.style.overflow = 'hidden';
+          }
+          return true;
+        })();`}
+      />
     </View>
   );
 }
@@ -14,7 +34,15 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    height:'100%',
+    padding:'75px',
+    width:'100%',
+    position:'absolute',
+   
+    left:'0',
+     // Ensures full black background
+  },
+  webview: {
+    flex: 1,
   },
 });
